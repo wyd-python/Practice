e0a37862abc6d489963a46ee9bf803d92036f6d4
playcrab <playcrab@playcrabdeMacBook-Air-8.local>
2018-11-09 10:19:47 +0800

    sad

---
+++ python.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+"""
+print('---------王亚东--------')
+guess = input("猜下我在想啥: ")
+if guess == "8":
+    print("我草，你猜对了!")
+    print("哼，猜对也没有用")
+else:
+    print ("错啦")
+print("游戏结束")
+"""
+"""
+s = "let's go"
+print (s)
+
+s1 = "C:\\won"
+print (s1)
+
+s2 = "isd \r\nsdw"
+print (s2)
+"""
+
+"""
+name = input("你叫啥啊： ")
+s = "我喜欢你很久了，%s" % name
+s1 = "我爱你，{}".format(name)
+print (s)
+print (s1)
+"""
+"""
+a = 10
+b = 20
+c = a == b
+print(c)
+"""
